11f20d02c148a6219f18b1ff717c06b541d8e4ff
matejslovak <joggurtik1@gmail.com>
2019-11-11 09:32:19 +0100

    done

--- Open-Lab-03.10/Checker.cs
+++ Open-Lab-03.10/Checker.cs
@@ -6,7 +6,16 @@ namespace Open_Lab_03._10
     {
         public int GetNumberOfCharsInString(char letter, string str)
         {
-            throw new NotImplementedException();
+            int b = 0;
+            foreach (char a in str)
+            {
+                if (a == letter)
+                    b = b + 1;
+               
+
+            }
+            return b;
+             
         }
     }
 }
